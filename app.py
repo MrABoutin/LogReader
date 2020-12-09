602ce059770c7c9f906dce3f04f16af8f08ed756
Alex Boutin <alex@cinetic.ca>
2020-12-09 12:20:46 -0500

    folder check

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from os import path
 import json
 import os
 
@@ -18,7 +19,11 @@ def read_file(file_path):
 
 def read_files(folder_path):
     logs = []
-    for file in os.listdir(folderPath):
+    if not path.exists(folder_path):
+        print("No Logs folder found")
+        return None
+
+    for file in os.listdir(folder_path):
         if file.endswith(".log") or file.endswith(".json"):
             logs += read_file(f"{folder_path}{file}")
     print(len(logs))
